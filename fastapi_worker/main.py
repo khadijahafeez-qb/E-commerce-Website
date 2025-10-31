ede3a4e3352183f294c9e68bc15d1b0cc777e4aa
khadijahafeez-qb <khadija.hafeez@qbatch.com>
2025-10-31 15:06:12 +0500

    remove fastapi_worker from this project and set as separate project

--- fastapi_worker/main.py
+++
@@ -1,9 +0,0 @@
-# fastapi_worker/main.py
-from fastapi import FastAPI
-from fastapi_worker.db import SessionLocal
-from fastapi_worker.models import OrderStats
-from sqlalchemy.orm import Session
-from sqlalchemy import desc
-
-app = FastAPI()
-
--- fastapi_worker/run_task.py
+++
@@ -1,6 +0,0 @@
-# fastapi_worker/run_task.py
-from fastapi_worker.tasks import calculate_order_stats
-
-if __name__ == "__main__":
-    result = calculate_order_stats.delay()
-    print("📤 Task queued. Task ID:", result.id)
